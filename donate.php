2d0faada6a7a3f6c84f91dd2edbf3cd0ec45a69f
roshanverma8652 <66140395+roshanverma8652@users.noreply.github.com>
2021-05-20 14:36:39 +0530

    Add files via upload

---
+++ donate.php
@@ -0,0 +1,80 @@
+
+<!DOCTYPE html>
+	<head>
+	<meta charset="utf-8">
+	<title>Covid-19 Donation</title>
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/superfish.css">
+
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+	</head>
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+		<header id="fh5co-header-section" class="sticky-banner">
+			<div class="container">
+				<div class="nav-header">
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
+					<h1 id="fh5co-logo"><a href="index.html">Covid-19 Donation</a></h1>
+					<!-- START #fh5co-menu-wrap -->
+					<nav id="fh5co-menu-wrap" role="navigation">
+						<ul class="sf-menu" id="fh5co-primary-menu">
+							<li class="active">
+								<a href="index.php">Home</a>
+							</li>
+						
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		
+		
+
+		<div class="fh5co-hero">
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(images/cover.jpg);">
+				<div class="desc animate-box">
+				<i><h3 style="color:white;">Tough Times!</h3></i>
+					
+					<p style="color:white;">We all are facing crisis due to this pandemic but The poor families are going through alot more than us</p>
+					<P style="color:white;">These People are suffering not only from Covid-19 but also from hunger</p>
+					<h2><strong>DONATE A SMILE</strong></h2>
+					<form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HAfHDPMCIk5gez" async> </script> </form>
+				</div>
+			</div>
+
+		</div>
+
+		<footer>
+			<div id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 text-center">
+							<p>Made by Roshan Verma</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	
+
+	</div>
+
+	</div>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/sticky.js"></script>
+	<script src="js/jquery.stellar.min.js"></script>
+	<script src="js/superfish.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
